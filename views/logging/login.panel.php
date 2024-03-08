f1058276d22009984efe3746640b73198bf39fb4
FrontMaybeBackend <krzysztof231201@interia.pl>
2024-03-08 13:44:13 +0100

    css for register and login forms

--- views/logging/login.panel.php
+++ views/logging/login.panel.php
@@ -13,18 +13,38 @@ $userLogin->loginUser();
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <link rel="stylesheet" href="../../styles/login.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Anta&display=swap" rel="stylesheet">
 </head>
 <body>
-<form action="" method="POST">
-    <label for="email">Email</label><br>
-    <input type="email" id="email" name="email" required><br>
-    <label for="password">Password:</label><br>
-    <input type="password" id="password" name="password" required><br>
-    <input type="submit" value="Zaloguj">
-</form>
-<button type="submit">
-<a href="../register/register.panel.php ">Zarejestruj sie</a>
-</button>
+
+
+
+
+<main>
+    <div id="container">
+        <div id="form">
+            <form action="" method="POST">
+                <div class="input-container">
+                <input type="email" id="email" name="email" required placeholder="Email"><br>
+                    <i class="fa fa-envelope-o icon"></i>
+                </div>
+                <div class="input-container">
+                <input type="password" id="password" name="password" required placeholder="Password"><br>
+                    <i class="fa fa-lock icon"></i>
+                </div>
+                <input id="login" type="submit" value="Zaloguj">
+                    <p>Nie masz konta?
+                    <a type="submit" href="../register/register.panel.php ">Zarejestruj sie</a>
+                    </p>
+            </form>
+
+        </div>
+    </div>
+</main>
 
 
 </body>
--- views/register/register.panel.php
+++ views/register/register.panel.php
@@ -1,7 +1,10 @@
 <?php
+
+use controllers\RegisterAccountController;
+
 require_once __DIR__ . "/../../includes/autoloader.php";
 
-$controller = new \controllers\RegisterAccountController('name','surname','password','email','phone');
+$controller = new RegisterAccountController('name', 'surname', 'password', 'email', 'phone');
 $controller->newAccount();
 ?>
 <!DOCTYPE html>
@@ -9,22 +12,42 @@ $controller->newAccount();
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <link rel="stylesheet" href="../../styles/register.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Anta&display=swap" rel="stylesheet">
 </head>
 <body>
-<form action="register.panel.php?success" method="POST">
-    <label for="name">First name:</label><br>
-    <input type="text" id="name" name="name" maxlength="12" required><br>
-    <label for="surname">Last name:</label><br>
-    <input type="text" id="surname" name="surname" maxlength="12" required><br>
-    <label for="email">Email:</label><br>
-    <input type="email" id="email" name="email" required><br>
-    <label for="password">Password:</label><br>
-    <input type="password" id="password" name="password" required><br>
-    <label for="phone">Phone:</label><br>
-    <input type="text" id="phone" name="phone"><br>
-    <input type="submit" value="Zarejestruj">
-</form>
-
+<main>
+    <div id="container">
+        <div id="form">
+            <form action="register.panel.php?success" method="POST">
+                <div class="input-container">
+                    <input type="text" id="name" name="name" maxlength="12" required placeholder="First name"><br>
+                    <i class="fa fa-user icon"></i>
+                </div>
+                <div class="input-container">
+                    <input type="text" id="surname" name="surname" maxlength="12" required placeholder="Last name"><br>
+                    <i class="fa fa-user icon"></i>
+                </div>
+                <div class="input-container">
+                    <input type="email" id="email" name="email" required placeholder="Email"><br>
+                    <i class="fa fa-envelope-o icon"></i>
+                </div>
+                <div class="input-container">
+                    <input type="password" id="password" name="password" required placeholder="Password"><br>
+                    <i class="fa fa-lock icon"></i>
+                </div>
+                <div class="input-container">
+                    <input type="text" id="phone" name="phone" placeholder="Phone"><br>
+                    <i class="fa fa-phone icon"></i>
+                </div>
+                <input id="register" type="submit" value="Zarejestruj">
+            </form>
+        </div>
+    </div>
+</main>
 
 </body>
 </html>
